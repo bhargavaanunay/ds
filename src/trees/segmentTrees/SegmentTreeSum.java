f5eab97b2b57c206522ff995cf8b29b027c1f298
bhargavaanunay <bhargava.anunay@gmail.com>
2017-02-14 11:04:54 +0530

    Added comments

--- src/trees/segmentTrees/SegmentTreeSum.java
+++ src/trees/segmentTrees/SegmentTreeSum.java
@@ -1,12 +1,22 @@
 package trees.segmentTrees;
 
 /**
+ * Basic Implementation of segment tree to query for sum in a given range.
+ * Creation take : O(nLogn)
+ * Get sum : O(Logn)
+ * Update : O(Logn)
  * Created by anunay on 14/02/17.
  */
 
 public class SegmentTreeSum {
     private int treeData[];
 
+    /**
+     * Creates the segment tree based on the given input array
+     *
+     * @param data Array that needs to converted
+     * @param n    Size of the array
+     */
     public SegmentTreeSum(int data[], int n) {
         int h = (int) Math.ceil(Math.log(n) / Math.log(2));
         int maxSize = (int) (2 * Math.pow(h, 2) - 1);
@@ -23,6 +33,14 @@ public class SegmentTreeSum {
         System.out.println(segmentTreeSum.getSum(0, n - 1, 1, 3, 0));
     }
 
+    /**
+     * Update a specified index in the array and the tree
+     *
+     * @param arr    data array
+     * @param n      size of array
+     * @param i      index of array that needs to be replaced
+     * @param newVal new value
+     */
     public void update(int arr[], int n, int i, int newVal) {
         int diff = newVal - arr[i];
         arr[i] = newVal;
@@ -52,6 +70,16 @@ public class SegmentTreeSum {
         return treeData[currentNode];
     }
 
+    /**
+     * Get sum for a specified range
+     *
+     * @param start       start index of the tree data
+     * @param end         end index of the tree data
+     * @param rangeStart  query start index
+     * @param rangeEnd    query end index
+     * @param currentNode starting node of the tree data
+     * @return sum of the range
+     */
     public int getSum(int start, int end, int rangeStart, int rangeEnd, int currentNode) {
         if (rangeStart <= start && rangeEnd >= end) {
             return treeData[currentNode];
